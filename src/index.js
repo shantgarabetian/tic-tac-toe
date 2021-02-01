bb2aadece34ff51c8cfacd203acb203e5a003519
Shant Garabetian <shantgarabetian@yahoo.com>
2021-01-31 21:11:04 -0800

    added onClick event handler,that show X on the game board

--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  onClick = () => {
+    this.setState({
+      value: "X",
+    });
+  };
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button 
+         className="square" 
+        onClick={this.onClick}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
